d1eaa2450ddb0f1e7ada05e26df2218f70506f6c
Bob In Vietnam <121599700+BobInVietnam@users.noreply.github.com>
2023-10-11 23:46:00 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.yranoitcid{
     requires transitive javafx.fxml;
     requires java.sql;
     requires json.simple;
-    requires org;
 
     opens com.yranoitcid.Frontend to javafx.fxml;
     exports com.yranoitcid.Frontend;
