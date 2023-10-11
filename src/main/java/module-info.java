5c4fb81481d531c149a16551fe478e72a040d4d1
egnaro1007 <ndviettuan@gmail.com>
2023-10-11 23:01:40 +0700

    Install Json Simple

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module com.yranoitcid.yranoitcid {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires json.simple;
 
     opens com.yranoitcid.yranoitcid to javafx.fxml;
     exports com.yranoitcid.yranoitcid;
 }
-
