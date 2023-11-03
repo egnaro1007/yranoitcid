465af80143d4fec38199fb6b88bd8d52fdaecf3d
egnaro1007 <ndviettuan@gmail.com>
2023-11-03 08:51:12 +0700

    Rebuilt UI

--- src/main/java/com/yranoitcid/frontend/MothershipController.java
+++ src/main/java/com/yranoitcid/frontend/MothershipController.java
@@ -2,16 +2,27 @@ package com.yranoitcid.frontend;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class MothershipController implements Initializable{
 
@@ -26,7 +37,12 @@ public class MothershipController implements Initializable{
     private List<String> resourceLink = new ArrayList<>();
 
     @FXML
-    private VBox mothership;
+    private HBox mothership;
+
+    @FXML
+    private Label time;
+    @FXML
+    private Label date;
 
     /**
      *  Initialize the app.
@@ -52,22 +68,33 @@ public class MothershipController implements Initializable{
         }
 
         System.out.println("Opening menu initialized successfully.");
+
+        // Live clock
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO,e -> {
+                String timeFormat = ((LocalDateTime.now().getNano() / 600_000_000) % 2 == 0) ? "HH:mm:ss" : "HH mm ss";
+                time.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern(timeFormat)));
+                date.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+            }),
+                new KeyFrame(Duration.seconds(0.1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
     }
 
     public void switchToDictionary() {
-        mothership.getChildren().set(0, menus.get(Menus.DICTIONARY.ordinal()));
+        mothership.getChildren().set(1, menus.get(Menus.DICTIONARY.ordinal()));
     }
 
     public void switchToTranslator() {
-        mothership.getChildren().set(0, menus.get(Menus.TRANSLATOR.ordinal()));
+        mothership.getChildren().set(1, menus.get(Menus.TRANSLATOR.ordinal()));
     }
 
     public void switchToEditor() {
-        mothership.getChildren().set(0, menus.get(Menus.EDITOR.ordinal()));
+        mothership.getChildren().set(1, menus.get(Menus.EDITOR.ordinal()));
     }
 
     public void switchToWordChain() {
-        mothership.getChildren().set(0, menus.get(Menus.WORDCHAIN.ordinal()));
+        mothership.getChildren().set(1, menus.get(Menus.WORDCHAIN.ordinal()));
     }
 
 }
